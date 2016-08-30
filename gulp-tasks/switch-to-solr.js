2a34b5809ca029c8f6b20e34fe81d719faccd08b
Diego Saavedra San Juan <diego.saavedra.san.juan@liu.se>
2016-08-30 12:19:35 +0200

    First working version

--- gulp-tasks/switch-to-solr.js
+++ gulp-tasks/switch-to-solr.js
@@ -9,10 +9,22 @@ var path = require("path");
 var fs = require("fs")
 
 gulp.task("switch-to-solr", function (callback) {
-  build.logEvent("builder", "Installing Solr version " + config.solrVersion);
-  var psFile = path.join(path.dirname(fs.realpathSync(__filename)), "../powershell-scripts/install-solr.ps1");
-  powershell.runAsync(psFile, " -sitecoreVersion '" + build.sitecoreVersion + "'" + " -webRootPath " + config.websiteRoot + " -packageLocation " + build.packageSourceLocation + " -packageName " + build.packageName, callback);
-  build.logEvent("builder", "Switching Sitecore instance on  " + config.websiteRoot " to use Solr");
-  var psFile = path.join(path.dirname(fs.realpathSync(__filename)), "../powershell-scripts/configure-sitecore-solr.ps1");
-  powershell.runAsync(psFile, " -sitecoreVersion '" + build.sitecoreVersion + "'" + " -webRootPath " + config.websiteRoot + " -packageLocation " + build.packageSourceLocation + " -packageName " + build.packageName, callback);
+  build.logEvent("builder", "Installing Solr 4.10.4");
+  
+  // Can be done with gulp-run? https://www.npmjs.com/package/gulp-run
+  var taskDir = path.dirname(fs.realpathSync(__filename));
+  var psFile = path.join(taskDir, "../powershell-scripts/install-solr.ps1");
+  var result = powershell.runSync(psFile, " -solrExtractLocation " + config.solrExtractLocation, path.join(taskDir, "../powershell-scripts"), callback);
+  if (result.status > 0)
+  {
+    build.LogEvent("builder", "Installing Solr failed, quitting")
+    process.exit()
+  }    
+  build.logEvent("builder", "Switching Sitecore instance on  " + config.websiteRoot + " to use Solr");
+  var psFile = path.join(taskDir, "../powershell-scripts/configure-sitecore-solr.ps1");
+  var result = powershell.runSync(psFile, " -webRootPath " + config.websiteRoot + " -solrExtractLocation " + config.solrExtractLocation, path.join(taskDir, "../powershell-scripts"), callback);
+  if (result.status > 0) {
+    build.LogEvent("builder", "Configuring Solr failed, quitting")
+    process.exit()
+  }
 });
