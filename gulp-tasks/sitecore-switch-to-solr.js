2a084fade4ec517a8d3bb6319032eeadeee56734
Emil Okkels Klein <eok@pentia.dk>
2016-10-18 13:51:17 +0200

    added set env to always be run when running sitecore-switch-to-solr alone

--- gulp-tasks/sitecore-switch-to-solr.js
+++ gulp-tasks/sitecore-switch-to-solr.js
@@ -8,6 +8,14 @@ var powershell = require("../modules/powershell");
 var path = require("path");
 var fs = require("fs")
 
+var nopt = require("nopt");
+
+var args = nopt({
+  "env"     : [String, null]
+});
+
+build.setEnvironment(args.env);
+
 gulp.task("sitecore-switch-to-solr", function (callback) { 
   // Can be done with gulp-run? https://www.npmjs.com/package/gulp-run
   var taskDir = path.dirname(fs.realpathSync(__filename));
