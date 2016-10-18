9b28f99a2039d1d0de60cb004d7ec2b3ed8ee81f
Emil Okkels Klein <eok@pentia.dk>
2016-10-18 14:49:31 +0200

    added same fix for install-solr task

--- gulp-tasks/install-solr.js
+++ gulp-tasks/install-solr.js
@@ -7,6 +7,12 @@ var config = build.config;
 var powershell = require("../modules/powershell");
 var path = require("path");
 var fs = require("fs")
+var nopt = require("nopt");
+var args = nopt({
+  "env"     : [String, null]
+});
+
+build.setEnvironment(args.env);
 
 gulp.task("install-solr", function (callback) {  
   // Can be done with gulp-run? https://www.npmjs.com/package/gulp-run
